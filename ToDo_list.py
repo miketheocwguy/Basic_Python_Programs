3627a3321b3845a07e3a1e77f8c4f84c313c091c
Michael Charles Nolan <48270695+miketheocwguy@users.noreply.github.com>
2023-07-02 12:10:27 -0400

    Update ToDo_list.py

--- ToDo_list.py
+++ ToDo_list.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit, completed or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -9,13 +9,17 @@ while True:
             todo = input("Enter TODO: ")
             todos.append(todo)
         case 'show':
-            for item in todos :
-                print(item)
+            for index , item in enumerate(todos) :
+                print(index+1,": ",item)
         case 'edit':
             number = int(input("number of the todo you want to edit: "))
             new_todo = input("enter new todo: ")
             todos[number-1] = new_todo
+        case 'completed':
+            number = int(input("Number of the completed item: "))
+            todos.pop(number-1)
+
         case 'exit':
             break
 
-print("bye")
+print("Thank you ")
